beb024318ca606ffea254c0fa7bb13dd91cdd51d
juanjociber <72362179+juanjociber@users.noreply.github.com>
2021-01-22 17:16:09 -0500

    Agregamos proyecto

---
+++ js/animacion.js
@@ -0,0 +1,6 @@
+$("#idFoto").mouseover(function(){
+    $("#idFoto").css({"transition":"all 1.5s","border-radius":"50%"});
+});
+$("#idFoto").mouseout(function(){
+    $("#idFoto").css("border-radius","1.2em 0px 1.2em 0px");
+});
